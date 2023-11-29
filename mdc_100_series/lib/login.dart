a9d5166074d283ad182a6025bca923cbba5e469b
sriram-ac <sriram@atomcare.in>
2023-11-29 12:52:24 +0530

    Add custom themes @app.dart

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -22,7 +22,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,10 +36,56 @@ class _LoginPageState extends State<LoginPage> {
               children: <Widget>[
                 Image.asset('assets/diamond.png'),
                 const SizedBox(height: 16.0),
-                const Text('SHRINE'),
+                Text(
+                  'Atom Health',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
               ],
             ),
             const SizedBox(height: 120.0),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                // filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 12.0),
+
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                // filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+
+            OverflowBar(
+              alignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _passwordController.clear();
+                    Navigator.pop(context);
+                  },
+                  style: TextButton.styleFrom(
+                    primary: Theme.of(context).colorScheme.secondary,
+                  ),
+                ),
+                // TODO: Add an elevation to NEXT (103)
+                // TODO: Add a beveled rectangular border to NEXT (103)
+                ElevatedButton(
+                  child: const Text('NEXT'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+
             // TODO: Remove filled: true values (103)
             // TODO: Add TextField widgets (101)
             // TODO: Add button bar (101)
